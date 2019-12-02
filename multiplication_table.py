79672246575fcd0958e83edca853d979c2b94218
vignesh-hub <58372359+vignesh-hub@users.noreply.github.com>
2019-12-02 12:43:52 +0530

    multiplication table
    print the multiplication table

---
+++ multiplication_table.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 30 23:28:55 2019
+
+@author: DELL
+"""
+#for example i am taking 10
+#own design actually we need to give 11 for c to print up to length 10
+#to solve that i give c=c+1,so user can give 10 itself
+#normal person can use this code.....
+print("multiplication table")
+a=input("which number table you needed: ")
+b=int(a)
+n=input("up to how much u need the table tell us the lenght :")
+c=int(n)
+c=c+1
+for i in range (c):
+    print(i,"x",b,"=",i*b)
